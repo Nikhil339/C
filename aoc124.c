94b9af7ad7e2aa2b252eea259e852ce804cf6813
Nikhil <revoor.nikhil@gmail.com>
2025-02-17 21:38:13 +0530

    learnt further about functions, started advent of code 2024

---
+++ aoc124.c
@@ -0,0 +1,7 @@
+//read the numbers from the file
+//declare two arrays, one for left and another for right
+//parse through the numbers and save the left column numbers in left array and similarly for the right
+//find the minimum value from both the arrays and calculate their difference
+//store the difference somewhere
+//remove the minimum values once the difference is calculated
+//now repeast the process until there are no elements in the array
