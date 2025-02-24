5370838b2fe9e9532d018e3f061180e85436ef05
Nikhil <revoor.nikhil@gmail.com>
2025-02-24 22:55:37 +0530

    started arrays and pointers

--- test.c
+++ test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include <time.h>
+
+int sum(int a[], int n, int *p);
 
 int main(void){
-    long t = time(NULL);
-    printf("%ld", t);
+    int N, *p, s;
+    printf("Enter number of elements: ");
+    scanf("%d", &N);
+    int b[N];
+    printf("Enter %d numbers: ", N);
+    for (int i=0;i<N;i++){
+        scanf("%d", &b[i]);
+    }
+    s = sum(b, N, p);
+    printf("The sum is: %d", s);
     return 0;
+}
+
+int sum(int a[], int n, int *p){
+    int s = 0;
+    for (p = &a[0];p<&a[n];p++){
+        s += *p;
+    }
+    return s;
 }
